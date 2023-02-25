acdcf41d7139ae1f66326141bd15eb7132b296c9
bharatrai53 <bharatrai2016@gmail.com>
2023-02-25 01:35:54 -0800

    fixed new files

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,7 @@ function About() {
     return (
         <div>
             <h1>About Me</h1>
-            <img src="../../../assets/bharatpic.jpg" alt="Bharat Rai, React Developer" />
+            <img src="./assets/bharatpic.jpg" alt="Bharat Rai, React Developer" />
             <p>Hi there! My name is Bharat Rai, and I'm a React Developer based in Los Angeles.</p>
             <p>I am currently working on building Ethos Health, a mobile application to help social drinkers monitor their alcohol consumption.</p>
             <p>When I'm not working on web development projects, I enjoy learning about healthcare financial management.</p>
